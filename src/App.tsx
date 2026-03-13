b0ffd37667e3ff682b67a1e0b713e4eb801f7733
Tung Pham <tung.42@gmail.com>
2026-03-13 18:15:59 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App: React.FC = () => {
           colorPrimary: "#ff7a45",
           borderRadius: 16,
           colorBgContainer: "#ffffff",
-          fontFamily: "'Anton', 'Gluten', sans-serif",
+          fontFamily: "'Gluten', 'Comic Sans MS', cursive",
         },
       }}
     >
